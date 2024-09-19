b4f427130a8c3d4847dfd7fbf8651cb55fa79503
MursikVar <159559170+MursikVar@users.noreply.github.com>
2024-09-19 20:15:33 +0300

    Faq

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,13 +2,14 @@ import { FirstList } from "./firstlist/FirstList";
 import { InfoCard } from "./infoCard/InfoCards";
 import { Fond } from "./fond/Fond";
 
+
 import { Form } from "./form/Form";
 import { Blogs } from "./blog/Blogs";
 
 
+import { Faq } from "./information/questions/Faq";
 import { AboutCompany } from "./information/aboutCompany/AboutCompany";
 import { Exchangeer } from "./information/exchangers/Exchangers";
-// import { Faq } from "./information/questions/Form";
 
 function Main (){
     return <>
@@ -17,12 +18,12 @@ function Main (){
         <Fond />
         <AboutCompany />
         <Exchangeer />
+        <Faq />
 
         <Form/>
         <Blogs />
 
 
-        {/* <Faq /> */}
     </>
 }
 
