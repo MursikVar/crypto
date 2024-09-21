711e6bf0c4313253093b188b769f13d570114bab
MursikVar <159559170+MursikVar@users.noreply.github.com>
2024-09-21 22:48:54 +0300

    finish

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,26 +2,26 @@ import { FirstList } from "./firstlist/FirstList";
 import { InfoCard } from "./infoCard/InfoCards";
 import { Fond } from "./fond/Fond";
 
+import { Information } from "./information/Inormation";
+
 
 import { Form } from "./form/Form";
 import { Blogs } from "./blog/Blogs";
 
 
-import { Faq } from "./information/questions/Faq";
-import { AboutCompany } from "./information/aboutCompany/AboutCompany";
-import { Exchangeer } from "./information/exchangers/Exchangers";
+
 
 function Main (){
     return <>
         <FirstList />
         <InfoCard />
         <Fond />
-        <AboutCompany />
-        <Exchangeer />
-        <Faq />
+
+        <Information />
 
         <Form/>
         <Blogs />
+
 
 
     </>
